2ea10ac5c709948539987dc21a466e1bda0cb3a2
Arshath01 <ahamedarshath35@gmail.com>
2024-01-17 22:20:08 +0530

    navbar fixed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -36,13 +36,24 @@ export default function Login() {
           onError={handleLoginFailure}
         />
       ) : (
-        <div className="flex space-x-4 items-center justify-end w-full">
+        <div
+          className="flex space-x-2 cursor-pointer items-center justify-end w-full"
+          onClick={() => triggerMenu()}
+        >
           <img
             src={user.picture}
             className="rounded-full h-10 w-10"
             alt="profile"
-            onClick={() => triggerMenu()}
           />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            width="20"
+            viewBox="0 0 512 512"
+            fill="white"
+          >
+            <path d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z" />
+          </svg>
         </div>
       )}
     </div>
