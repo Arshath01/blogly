713583270bd9ba4c5142f2113a9ce6b1e669bb0e
Arshath01 <ahamedarshath35@gmail.com>
2024-01-22 08:03:29 +0530

    data fetching fixed

--- src/components/Blog/BlogParent.jsx
+++ src/components/Blog/BlogParent.jsx
@@ -1,20 +1,20 @@
 import Post from "./Post";
 import Category from "./Category";
 import { useEffect, useState } from "react";
-import { useUser } from "../../context/useContext";
+// import { useUser } from "../../context/useContext";
 
 export default function BlogParent() {
   const [selectedCat, setSelectedCat] = useState();
-  const user_exist = localStorage.getItem("authToken");
+  // const user_exist = localStorage.getItem("authToken");
 
   useEffect(() => {
     window.scroll(0, 0);
-    function fetchData() {
-      fetch("http://localhost:3001/api/getposts")
-        .then((response) => response.json().then((data) => console.log(data)))
-        .catch((err) => console.log(err));
-    }
-    fetchData();
+    // function fetchData() {
+    //   fetch("http://localhost:3001/api/getposts")
+    //     .then((response) => response.json().then((data) => console.log(data)))
+    //     .catch((err) => console.log(err));
+    // }
+    // fetchData();
   }, []);
 
   function categoryHandler(category) {
