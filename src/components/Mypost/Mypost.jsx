c7a5b8acecdbaa508ddd9a46d82394407c89c396
Arshath01 <ahamedarshath35@gmail.com>
2024-02-08 19:39:36 +0530

    fix mypost

--- src/components/Mypost/Mypost.jsx
+++ src/components/Mypost/Mypost.jsx
@@ -2,42 +2,42 @@ import React, { useEffect, useState } from "react";
 import { useUser } from "../../context/useContext";
 import Post from "../Blog/Post";
 import Loading from "../Loading/Loading";
-import { testData } from "../Blog/demoData";
+// import { testData } from "../Blog/demoData";
 
 export default function Mypost() {
   const { user } = useUser();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     console.log(user);
-  //     try {
-  //       const response = await fetch(
-  //         `https://blogly-api.vercel.app/api/mypost/${user.sub}`
-  //       );
+  useEffect(() => {
+    async function fetchData() {
+      console.log(user);
+      try {
+        const response = await fetch(
+          `https://blogly-api.vercel.app/api/mypost/${user.sub}`
+        );
 
-  //       if (response.ok) {
-  //         const data = await response.json();
-  //         setData(data);
-  //         setLoading(false);
-  //         console.log(data);
-  //       } else {
-  //         console.error("Failed to fetch data");
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, [user]);
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+          setLoading(false);
+          console.log(data);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, [user]);
 
-  return !loading ? (
+  return loading ? (
     <Loading />
   ) : (
     <div className="mt-4">
       {user && <h2 className="text-lg font-bold">Welcome, {user.name} </h2>}
-      <Post myPost={testData} />
+      <Post myPost={data} />
     </div>
   );
 }
